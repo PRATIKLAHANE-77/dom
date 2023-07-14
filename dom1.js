0aa4ed576ca44d4eb562e012e9a43ac5067bbc3b
PRATIKLAHANE-77 <pratik.lahane1999@gmail.com>
2023-07-14 18:42:55 +0530

    i am able to understand dom

---
+++ dom1.js
@@ -0,0 +1,10 @@
+var usingtag = document.getElementsByTagName('li');
+// usingtagstyle[4].backgroundColor = 'green';
+//  for(let i = 0;i<usingtag.length;i++) {
+//     usingtag[i].style.backgroundColor = "red";
+//  }
+var usingclass = document.getElementsByClassName('list-group-item');
+for(let i = 0;i<usingclass.length;i++) {
+    usingclass[i].style.backgroundColor = "red";
+}
+ 
